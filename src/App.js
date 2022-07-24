31ce5ab78197599d28e0dcf207140a264b0d9243
awaisrehmat <100486479+awaisrehmat@users.noreply.github.com>
2022-07-24 18:57:56 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes , Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes , Route , Redirect} from "react-router-dom";
 
 import LanPage from "./components/LandingPage";
 import TopMenu from "./components/TopBar";
 import Contact from "./components/Contact";
 import Products from "./components/Products";
+import PageNotFound from "./components/NotFound";
 
 
 function App() {
@@ -15,10 +16,11 @@ function App() {
     <Routes>
    <Route exact path="/contact-us" element={<Contact/>}/>
     <Route exact path="/products" element={<Products/>}/>
-    <Route exact path="/" element={<LanPage/>}/>
-        
-    </Routes>
     
+    <Route exact path="/" element={<LanPage/>}/>
+
+    <Route path="*" element={<PageNotFound/>}/>
+   </Routes>
  </div>
 
  </Router>
